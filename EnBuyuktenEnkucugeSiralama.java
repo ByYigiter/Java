bc04af192bcf5df2078f387ea4b584c40f389d6f
Yigit Er <mr.yigiter222@gmail.com>
2022-12-22 09:26:11 +0300

    sıralama

---
+++ EnBuyuktenEnkucugeSiralama.java
@@ -0,0 +1,28 @@
+package myOwnWork;
+
+import java.util.Arrays;
+
+public class EnBuyuktenEnkucugeSiralama {
+
+    public static void main(String[] args) {
+        int arr[] ={2,5,9,8,4};
+        System.out.println(Arrays.toString(arr));
+
+        int min=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++){
+                if (arr[j-1]<arr[j]) {
+                    min=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=min;
+                }
+            }
+
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
